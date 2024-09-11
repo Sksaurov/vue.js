9ae59ca8f3300cab9ad7659a01c6aad61f91523f
Saurov <114143617+Sksaurov@users.noreply.github.com>
2024-09-11 12:02:58 +0600

    Create 2about v-bind.js

---
+++ 2about v-bind.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        courseGoal: 'finish the course',
+        vueLink:'https://www.youtube.com/'
+      };
+    }
+  });
+  
+  app.mount('#user-goal');
